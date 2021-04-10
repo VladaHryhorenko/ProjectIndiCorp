b669e9592087ab4025a1acb1d967973e81032b53
VladaGriggy <mrsgriggy@gmail.com>
2021-04-10 03:27:23 +0300

    Adding 2 last sections + work with hovers in desktop and mobile versions

--- js/scripts.js
+++ js/scripts.js
@@ -18,18 +18,50 @@ $(document).ready(function(){
     $('.video-icon').on('click', () => {
         $('.video-icon').toggleClass('video-img');
         $('.video-overlay').toggleClass('video-img');
-        $("#video1")[0].src += "?autoplay=1";
-        $("#video2")[0].src += "?autoplay=1";
     })
 
     $('.video-overlay').on('click', () => {
         $('.video-icon').toggleClass('video-img');
         $('.video-overlay').toggleClass('video-img');
-        $("#video1")[0].src += "?autoplay=1";
-        $("#video2")[0].src += "?autoplay=1";
     })
 
+    function checkWidth() {
+        var windowWidth = $('body').innerWidth(),
+            elem = $('.video-icon');
+            elem1 = $('.support-link-request');
+            elem2 = $('.contact-link');
+            elem3 = $('.footer-nav-item a');
+            elem4 = $('.frst-link');
+            elem5 = $('.scnd-link');
+            elem6 = $('.nav-item');
+            elem7 = $('.subscribe-button');
+                                        
+        if(windowWidth < 1025){
+          elem.removeClass('video-icon-hover');
+          elem1.removeClass('hover-support-link-request');
+          elem2.removeClass('hover-class-link');
+          elem3.removeClass('hover-footer-nav-item');
+          elem4.removeClass('hover-link');
+          elem5.removeClass('hover-link');
+          elem6.removeClass('hover-nav-item');
+          elem7.removeClass('hover-subscribe-button');
+        }
+        else{
+          elem.addClass('video-icon-hover');
+          elem1.addClass('hover-support-link-request');
+          elem2.addClass('hover-class-link');
+          elem3.addClass('hover-footer-nav-item');
+          elem4.addClass('hover-link');
+          elem5.addClass('hover-link');
+          elem6.addClass('hover-nav-item');
+          elem7.addClass('hover-subscribe-button');
+        }
+      }
     
-
+      checkWidth(); // проверит при загрузке страницы
+    
+      $(window).resize(function(){
+        checkWidth(); // проверит при изменении размера окна клиента
+      });
 
 });
